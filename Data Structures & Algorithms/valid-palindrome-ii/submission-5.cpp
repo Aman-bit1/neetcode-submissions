1a87383cc9d197b4a339617d2e26a86dc05174fc
Aman Kumar <168061002+Aman-bit1@users.noreply.github.com>
2026-07-16 22:35:38 +0530

    Add: valid-palindrome-ii - submission-5

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-5.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+   bool ispal(string &s,int i,int j){
+    while(i<j){
+        if(s[i]!=s[j]) return false;
+        i++;
+        j--;
+    }
+    
+    return true;
+   }
+    bool validPalindrome(string s) {
+        int n=s.size();
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            }
+            else return (ispal(s,i+1,j)||ispal(s,i,j-1));
+        }
+ return true;
+    }
+};
